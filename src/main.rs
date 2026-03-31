9408db29b39a55db0db1280cf9bf3f7f5bb40051
marc55s <marc.schoenig@gmail.com>
2026-03-31 19:39:17 +0200

    update: better pins for my setup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
 use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyle},
-    prelude::{Point, Primitive, Size},
-    primitives::{PrimitiveStyle, Rectangle},
-    text::Text,
-    Drawable,
+    Drawable, mono_font::{MonoTextStyle, ascii::FONT_6X10, iso_8859_1::FONT_6X13}, prelude::{Point, Primitive, Size}, primitives::{PrimitiveStyle, Rectangle}, text::Text
 };
 use epd_waveshare::{
     color::Color,
@@ -11,12 +7,11 @@ use epd_waveshare::{
     prelude::{DisplayRotation, WaveshareDisplay},
 };
 use esp_idf_svc::hal::{
-    delay::Delay,
+    delay::{Delay, Ets, FreeRtos},
     gpio::{AnyInputPin, PinDriver, Pull},
     peripherals::Peripherals,
     spi::{
-        config::{Config, DriverConfig},
-        SpiDeviceDriver, SpiDriver,
+        SpiDeviceDriver, SpiDriver, config::{Config, DriverConfig}
     },
 };
 
@@ -28,13 +23,13 @@ fn main() {
     let peripherals = Peripherals::take().unwrap();
     let spi = peripherals.spi2;
 
-    let busy = PinDriver::input(peripherals.pins.gpio10, Pull::Floating).unwrap();
-    let dc = PinDriver::output(peripherals.pins.gpio9).unwrap();
-    let rst = PinDriver::output(peripherals.pins.gpio8).unwrap();
+    let busy = PinDriver::input(peripherals.pins.gpio4, Pull::Down).unwrap();
+    let dc = PinDriver::output(peripherals.pins.gpio16).unwrap();
+    let rst = PinDriver::output(peripherals.pins.gpio17).unwrap();
 
-    let sdo = peripherals.pins.gpio0;
-    let sclk = peripherals.pins.gpio1;
-    let cs = Some(peripherals.pins.gpio2);
+    let sclk = peripherals.pins.gpio18; // VSPI CLK
+    let sdo = peripherals.pins.gpio23; // VSPI MOSI
+    let cs = Some(peripherals.pins.gpio5); // VSPI CS
     let sdi: Option<AnyInputPin> = None;
 
     let spi_config = DriverConfig::new();
@@ -42,21 +37,25 @@ fn main() {
     let spi_driver_config = Config::default();
     let mut spi_device = SpiDeviceDriver::new(spi_driver, cs, &spi_driver_config).unwrap();
 
-    let mut epd = Epd2in13::new(&mut spi_device, busy, dc, rst, &mut Delay::new(1), None).unwrap();
-
+    let mut ets = Ets;
+    let mut epd = Epd2in13::new(&mut spi_device, busy, dc, rst, &mut ets, None).unwrap();
+    epd.clear_frame(&mut spi_device, &mut ets).unwrap();
     let mut display = Display2in13::default();
     display.set_rotation(DisplayRotation::Rotate90);
-    let _ = Rectangle::new(Point::new(0, 0), Size::new(250, 122))
+    let _ = Rectangle::new(Point::new(0, 0), Size::new(250, 130))
         .into_styled(PrimitiveStyle::with_fill(Color::White))
         .draw(&mut display);
 
-    let style = MonoTextStyle::new(&FONT_6X10, Color::Black);
+    let style = MonoTextStyle::new(&FONT_6X13, Color::Black);
     let _ = Text::new("Hello World", Point::new(0, 50), style).draw(&mut display);
 
-    let mut delay = Delay::new(10);
-    epd.update_frame(&mut spi_device, display.buffer(), &mut delay)
+    // let mut delay = Delay::new(10);
+    epd.update_frame(&mut spi_device, display.buffer(), &mut ets)
         .unwrap();
-    epd.display_frame(&mut spi_device, &mut delay).unwrap();
+    epd.display_frame(&mut spi_device, &mut ets).unwrap();
 
-    epd.sleep(&mut spi_device, &mut delay).unwrap();
+    epd.sleep(&mut spi_device, &mut ets).unwrap();
+    loop {
+        FreeRtos::delay_ms(1000);
+    }
 }
